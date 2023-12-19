9850549232d05e17c5371c7c8e78ddb2479fe665
ashif <ashif@LAPTOP-A45GL5JI>
2023-12-19 10:50:10 +0530

    project update access modi

---
+++ Test_Maven/src/main/java/SumMav/TestJa.java
@@ -0,0 +1,16 @@
+package SumMav;
+
+public class TestJa {
+	public static void main(String args[])
+	{
+		TestJa s= new TestJa();
+		TestJa s2= new TestJa(12);
+	}
+	public TestJa() {
+		
+	}
+	public TestJa(int a){
+		System.out.println("hi");
+	}
+
+}
