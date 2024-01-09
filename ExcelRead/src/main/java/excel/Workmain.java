8dc1bf39b167c83d18ad2b1e8126e76f77471c5a
ashif <ashif@LAPTOP-A45GL5JI>
2024-01-09 09:08:31 +0530

    work updates

--- ExcelRead/src/main/java/excel/Workmain.java
+++ ExcelRead/src/main/java/excel/Workmain.java
@@ -1,14 +1,15 @@
 package excel;
+
 import java.io.IOException;
 
 public class Workmain {
-
+    static String d;
 	public static void main(String[] args) throws IOException {
-		String d="name";
+		
 		for(int i=1;i<=8;i++) 
 		{
-		
 		d=WorkCode.readStringData(i, 0);
+				
 		System.out.println(d);
 			}
 			
